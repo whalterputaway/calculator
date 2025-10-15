3962fb91ba79196153ab94685a687585ff30af62
Vlad Chernov <vladchercash@dodo.com>
2025-10-15 22:46:39 +0300

    maybe final

--- main.py
+++ main.py
@@ -51,8 +51,10 @@ def button_delete_click():
     label_workpad["text"] = label_workpad["text"][0:-1]
 def button_procent_click():
     memory.append(float(label_workpad["text"]))
-    if what_to_do=="*": 
+    if what_to_do=="-" or what_to_do=="+": 
         label_workpad["text"] =(memory[0]*memory[1])*0.01
+    if what_to_do=="*" or what_to_do=="/":
+        label_workpad["text"]=memory[1]/100
     memory.clear()
 def button_CE_click():
     label_workpad["text"] = "0"
@@ -64,30 +66,30 @@ def comma_click():
         label_workpad["text"]+="." 
     else:
         pass
-button_sum = Button(text="+", width=10, height=2,font=(5),command=lambda: operation_click("+")).place(x=300,y = 250)
-button_sub = Button(text="-", width=10, height=2,font=(5),command=lambda: operation_click("-")).place(x=300,y=200)
-button_mul = Button(text="×", width=10, height=2,font=(5),command=lambda: operation_click("*")).place(x=300,y=150)
-button_div = Button(text="÷", width=10, height=2,font=(5),command=lambda: operation_click("/")).place(x=300,y =100)
-button_equal = Button(text="=",width=10,height=2,font=(5),command=lambda: equal_click(what_to_do)).place(x=300, y=300)
-button1 = Button(text="1", width=10, height=2,font=(5), command=lambda: digit_click(1)).place(x = 0, y = 250)
-button2 = Button(text="2", width=10, height=2,font=(5), command=lambda: digit_click(2)).place(x = 100, y = 250)
-button3 = Button(text="3", width=10, height=2,font=(5), command=lambda: digit_click(3)).place(x = 200, y = 250)
-button4 = Button(text="4", width=10, height=2,font=(5), command=lambda: digit_click(4)).place(x = 0, y = 200)
-button5 = Button(text="5", width=10, height=2, font=(5),command=lambda: digit_click(5)).place(x = 100, y = 200)
-button6 = Button(text="6", width=10, height=2, font=(5),command=lambda: digit_click(6)).place(x = 200, y = 200)
-button7 = Button(text="7", width=10, height=2,font=(5), command=lambda: digit_click(7)).place(x = 0, y = 150)
-button8 = Button(text="8", width=10, height=2,font=(5), command=lambda: digit_click(8)).place(x = 100, y = 150)
-button9 = Button(text="9", width=10, height=2, font=(5),command=lambda: digit_click(9)).place(x = 200, y = 150)
-button0 = Button(text="0", width=10, height=2,font=(5), command=lambda: digit_click(0)).place(x = 100, y = 300)
-button_comma = Button(text=".", width=10, height=2,font=(5),command=comma_click).place(x=200, y=300)
-button_negate = Button(text="+/-", width=10, height=2,font=(5),command=negate_click).place(x=0, y=300)
-button_sqrt = Button(text="√x", width=10, height=2,font=(5), command=sqrt_click).place(x=200, y=100)
-button_square = Button(text="x²", width=10, height=2,font=(5),command=square_click).place(x=100, y=100)
-button_1divx = Button(text="1/x", width=10, height=2, font=(5),command=click_1divx).place(x=0, y=100)
-button_procent = Button(text="%", width=10, height=2,font=(5), command=button_procent_click).place(x=0, y=50)
-button_CE = Button(text="CE", width=10, height=2,font=(5),command=button_CE_click).place(x=100, y=50)
-button_C = Button(text="C", width=10, height=2,font=(5),command=button_C_click).place(x=200, y=50)
-button_delete = Button(text="⌫", width=10, height=2,font=(5), command=button_delete_click).place(x=300, y=50)
+button_sum = Button(text="+", width=10, height=2,command=lambda: operation_click("+")).place(x=300,y = 250)
+button_sub = Button(text="-", width=10, height=2,command=lambda: operation_click("-")).place(x=300,y=200)
+button_mul = Button(text="×", width=10, height=2,command=lambda: operation_click("*")).place(x=300,y=150)
+button_div = Button(text="÷", width=10, height=2,command=lambda: operation_click("/")).place(x=300,y =100)
+button_equal = Button(text="=",width=10,height=2,command=lambda: equal_click(what_to_do)).place(x=300, y=300)
+button1 = Button(text="1", width=10, height=2, command=lambda: digit_click(1)).place(x = 0, y = 250)
+button2 = Button(text="2", width=10, height=2, command=lambda: digit_click(2)).place(x = 100, y = 250)
+button3 = Button(text="3", width=10, height=2, command=lambda: digit_click(3)).place(x = 200, y = 250)
+button4 = Button(text="4", width=10, height=2, command=lambda: digit_click(4)).place(x = 0, y = 200)
+button5 = Button(text="5", width=10, height=2, command=lambda: digit_click(5)).place(x = 100, y = 200)
+button6 = Button(text="6", width=10, height=2,command=lambda: digit_click(6)).place(x = 200, y = 200)
+button7 = Button(text="7", width=10, height=2,command=lambda: digit_click(7)).place(x = 0, y = 150)
+button8 = Button(text="8", width=10, height=2,command=lambda: digit_click(8)).place(x = 100, y = 150)
+button9 = Button(text="9", width=10, height=2, command=lambda: digit_click(9)).place(x = 200, y = 150)
+button0 = Button(text="0", width=10, height=2,command=lambda: digit_click(0)).place(x = 100, y = 300)
+button_comma = Button(text=".", width=10, height=2,command=comma_click).place(x=200, y=300)
+button_negate = Button(text="+/-", width=10, height=2,command=negate_click).place(x=0, y=300)
+button_sqrt = Button(text="√x", width=10, height=2,command=sqrt_click).place(x=200, y=100)
+button_square = Button(text="x²", width=10, height=2,command=square_click).place(x=100, y=100)
+button_1divx = Button(text="1/x", width=10, height=2, command=click_1divx).place(x=0, y=100)
+button_procent = Button(text="%", width=10, height=2,command=button_procent_click).place(x=0, y=50)
+button_CE = Button(text="CE", width=10, height=2,command=button_CE_click).place(x=100, y=50)
+button_C = Button(text="C", width=10, height=2,command=button_C_click).place(x=200, y=50)
+button_delete = Button(text="⌫", width=10, height=2,command=button_delete_click).place(x=300, y=50)
 main.bind('<F1>', show_help)
 label_workpad = Label(text="0",width=40,height=2,anchor="e",font=(100))
 label_workpad.place(x=5, y = 4)
